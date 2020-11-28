e7ff09caacb4656dbbd70bdf138fc713d65b94b5
wafaabengithub <73888792+wafaabengithub@users.noreply.github.com>
2020-11-28 03:17:07 +0100

    Add files via upload

---
+++ data/incomesData.js
@@ -0,0 +1,155 @@
+var incomeCategories = [{"page":"1","pages":"1","per_page":"10","total":"10"},
+[{"id":"HIC","value":"High income"},
+{"id":"high","value":"Heavily indebted poor countries (HIPC)"},
+{"id":"INX","value":"Not classified"},
+{"id":"LIC","value":"Low income"},
+{"id":"LMC","value":"Lower average income"},
+{"id":"LMY","value":"Low & average income"},
+{"id":"average","value":"average income"},
+{"id":"NOC","value":"High income: nonvery highD"},
+{"id":"very high","value":"High income: very highD"},
+{"id":"low","value":"Upper average income"}]];
+var emissions= [
+
+                    {
+                        "page": 1,
+                        "pages": 1,
+                        "per_page": "247",
+                        "total": 246
+                      
+                      },
+                      [
+              {
+                          "id": "MAR",
+                          "iso2Code": "MA",
+                          "name": "Morocco",
+                        
+                          "adminregion": {
+                            "id": "MEA",
+                            "value": "average East & North Africa (developing only)"
+                          },
+                          "emission": {
+                            "id": "high",
+                            "value": "16 100 - 75 982"
+                          },
+                       
+                          "capitalCity": "Rabat",
+                          "longitude": "-6.8704",
+                          "latitude": "33.9905"
+                        },
+
+
+                         {
+                          "id": "DZA",
+                          "iso2Code": "DZ",
+                          "name": "Algeria",
+                        
+                          "adminregion": {
+                            "id": "MNA",
+                            "value": "average East & North Africa (developing only)"
+                          },
+                          "emission": {
+                            "id": "high",
+                            "value": "  16 100 - 75 982"
+                          },
+                          
+                          "capitalCity": "Algiers",
+                          "longitude": "3.05097",
+                          "latitude": "36.7397"
+                        },
+                         {
+                          "id": "TUN",
+                          "iso2Code": "TN",
+                          "name": "Tunisia",
+                       
+                          "adminregion": {
+                            "id": "MNA",
+                            "value": "average East & North Africa (developing only)"
+                          },
+                          "emission": {
+                            "id": "average",
+                            "value": "6 100 -< 16 100"
+                          },
+                    
+                          "capitalCity": "Tunis",
+                          "longitude": "10.21",
+                          "latitude": "36.7899"
+                        },
+                        {
+                          "id": "EGY",
+                          "iso2Code": "EG",
+                          "name": "Egypt, Arab Rep.",
+                   
+                          "adminregion": {
+                            "id": "MNA",
+                            "value": "average East & North Africa (developing only)"
+                          },
+                          "emission": {
+                            "id": "very high",
+                            "value": "620 -< 2 900"
+                          },
+                     
+                          "capitalCity": "Cairo",
+                          "longitude": "31.2461",
+                          "latitude": "30.0982"
+                        },
+                        {
+                          "id": "EMU",
+                          "iso2Code": "XC",
+                          "name": "Euro area",
+                     
+                          "adminregion": {
+                            "id": "",
+                            "value": ""
+                          },
+                          "emission": {
+                            "id": "NA",
+                            "value": "Aggregates"
+                          },
+                    
+                          "capitalCity": "",
+                          "longitude": "",
+                          "latitude": ""
+                        },
+                         {
+                          "id": "LBY",
+                          "iso2Code": "LY",
+                          "name": "Libya",
+                  
+                          "adminregion": {
+                            "id": "MNA",
+                            "value": "average East & North Africa (developing only)"
+                          },
+                          "emission": {
+                            "id": "low",
+                            "value": "2 900 -< 6 100"
+                          },
+                    
+                          "capitalCity": "Tripoli",
+                          "longitude": "13.1072",
+                          "latitude": "32.8578"
+                        },
+                        {
+                          "id": "LCA",
+                          "iso2Code": "LC",
+                          "name": "St. Lucia",
+                    
+                          "adminregion": {
+                            "id": "LAC",
+                            "value": "Latin America & Caribbean (developing only)"
+                          },
+                          "emission": {
+                            "id": "low",
+                            "value": "Upper average income"
+                          },
+                      
+                          "capitalCity": "Castries",
+                          "longitude": "-60.9832",
+                          "latitude": "14"
+                        }
+
+
+                       
+
+                      ]
+                    ];
---
+++ data/solidFuels.js
@@ -0,0 +1,29 @@
+var solidFuels = [
+
+                  {
+                        'Vulnerability to drought (%)': "17.51",
+                        "CountryCode": "504",
+                        "Country": "Morocco"
+                  },
+                  {
+                        'Vulnerability to drought (%)': "7.11",
+                        "CountryCode": "012",
+                        "Country": "Algeria"
+                  },
+                  {
+                        'Vulnerability to drought (%)': "1.61",
+                        "CountryCode": "818",
+                        "Country": "Egypt"
+                  },
+                  {
+                        'Vulnerability to drought (%)': "1.66",
+                        "CountryCode": "434",
+                        "Country": "Libyan Arab Jamahiriya"
+                  },
+                  {
+                        'Vulnerability to drought (%)': "27.52",
+                        "CountryCode": "788",
+                        "Country": "Tunisia"
+                  },
+                  
+            ];
